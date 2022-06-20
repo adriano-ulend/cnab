b584198ab6b7b6a93d5e58c2635b135fe22795e6
adriano.coelho <adriano.coelho@ulend.com.br>
2022-06-20 18:39:52 -0300

    final statement of class params

--- cnab.py
+++ cnab.py
@@ -49,32 +49,41 @@ class CnabParser:
         cnpj = input_data['DOC_SACADO']
         format_cnpj_string = {".": "", "/": "", "-": ""}
         for key, value in format_cnpj_string.items():
-            cnpj = cnpj.replace(key, value)  # check algorithm to format cnpj data (only integers)
+            cnpj = cnpj.replace(key, value)  # formatted cnpj data (with only integers)
         fdic_data.append(cnpj)
 
-        item3_13 = input_data['NU_DOCUMENTO']
+        item3_13 = str(input_data['NU_DOCUMENTO'])
         fdic_data.append(item3_13)
 
-        item4_1a4_4 = "10000001"  # identificação = 1 ; data da carencia = 000000 ; tipo de juros = 1 - fixo
+        item4_1a4_4 = "10000001"  # identificação = 1 ; data da carencia = 000000 ; tipo de juros = 1(fixo)
         item4_17 = input_data['VALOR_NOMINAL']
         item4_5a4_20 = f"{0*10}02990215010199 B0             {item3_13}_070000000000000000000 {item4_17}  {self.date_now}{8*' '}"
+        fdic_data.append(item4_5a4_20)
 
         item4_24 = input_data['VALOR_NOMINAL']
-        item4_21a4_26 = f"15 {item3_13}{self.date_now}{7*0}{item4_24}{8*0}41"
+        item4_24f = item4_24.replace(',', '').replace('.', '')
+        item4_21a4_26 = f"15 {item3_13}{self.date_now}{7*0}{item4_24f}{8*0}41"
+        fdic_data.append(item4_21a4_26)
 
         item4_27a4_33 = f" {self.date_now}00002{12*0}{19*' '}"
+        fdic_data.append(item4_27a4_33)
 
         item4_34 = input_data['VALOR_AQUISICAO']
-        item4_34a4_41 = f"{7*0}{item4_34}{13*0}02{cnpj}{nome}<check endereço>{12*0}"
+        item4_34f = item4_34.replace(',', '').replace('.', '')
+        item4_34a4_41 = f"{7*0}{item4_34f}{13*0}02{cnpj}{nome}<check endereço>{12*0}"
+        fdic_data.append(item4_34a4_41)
+
+        # ! CHECK: verificar o input do endereco (originador, cedente ou sacado? qual fonte externa?)
 
         seq_registro = input_data['SEU_NUMERO']  # valor apos o underline
-        item4_42 = f"Ulend Gestao Financeira Ltda.32475607000114{100*0}{seq_registro}"
+        seq_registro_formatted = seq_registro.replace('_', '')
+        item4_42 = f"Ulend Gestao Financeira Ltda.32475607000114{29*' '}{71*0}{seq_registro_formatted}"
+        fdic_data.append(item4_42)
         # juros_mensal + juros_mora + multa + 71zeros
 
-        item5_1a5_3 = f"9{493*' '}{seq_registro}"
+        item5_1a5_3 = f"9{493*' '}{seq_registro_formatted}"
+        fdic_data.append(item5_1a5_3)
 
-        item4_34 = '444,00'
-        t2 = self.var2
         return fdic_data
 
     # def selected_table_data(self):
@@ -100,9 +109,16 @@ if __name__ == '__main__':
     #     print(cnab.ulend_data(cnab_data))
 
     for series in cnab_volume:
-        series = json.loads(series)
-        print(type(series))
-        teste5 = cnab.ulend_data(series)
-        print(teste5)
-        teste6 = cnab.fdic_data(series)
-        print(teste6)
+
+        titulo = cnab.title
+
+        with open(f'/home/adriano/dev/projects/cnab/docs/{titulo}.txt', 'r', enconding='utf-8') as file:
+
+            series = json.loads(series)
+            print(type(series))
+            teste5 = cnab.ulend_data(series)
+            print(teste5)
+            teste6 = cnab.fdic_data(series)
+            print(teste6)
+
+            file.write(teste5 + teste6)
